5dec091b1c757394c2a62891a2b0f15a6673594b
RonitJ <ronitjacobofficial@gmail.com>
2025-09-20 21:54:32 +0530

    Added a Prime number check source code.

---
+++ Calculator/src/Trials.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Trials
+{
+    public static void main(int a , int b, String args[], int ...v)
+    {
+        System.out.println(a+", "+b+", "+Arrays.toString(v));
+    }
+}
