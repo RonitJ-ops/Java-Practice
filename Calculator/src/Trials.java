b1b5f86c3e6985f8b35d731af559313c55b39904
RonitJ <ronitjacobofficial@gmail.com>
2025-09-22 23:46:12 +0530

    Armstrong Number checking program.

--- Calculator/src/Trials.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class Trials
-{
-    public static void main(int a , int b, String args[], int ...v)
-    {
-        System.out.println(a+", "+b+", "+Arrays.toString(v));
-    }
-}
